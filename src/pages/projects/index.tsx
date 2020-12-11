d39dec353c05ffa5af6f242c436317fd91f1f3ce
wesley guerra <guerra_ssa@hotmail.com>
2020-12-11 10:51:10 -0300

    update card2

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -32,6 +32,27 @@ const Projects: React.FC = () => {
                           <ButtonLink href="https://github.com/Wesleygmssa/estudos-nodejs-conceitos-gobaber">Back-End</ButtonLink>
                         </div>
                    </CardContent>
+              </Card>   
+
+                <Card>
+                    <img src={GobarberWeb} alt="GoBarber"/>
+                    <CardContent>
+                        <h3>GoBarber</h3>
+                        <p>Projeto criado do zero onde é feito o front-end e back-end, Utilizando as tecnologias</p>
+                        <ul>
+                          <a href="/">
+                            <FaReact size={40} style={{ color: "#7AE3F8" }} />
+                         </a>
+                          <a href="/">
+                            <FaNodeJs size={40} style={{ color: "#7CB701" }} />
+                          </a>
+                
+                        </ul>
+                        <div className="button-group">
+                          <ButtonLink href="https://github.com/Wesleygmssa/estudos-react-conceitos-gobaber">Front-End</ButtonLink>
+                          <ButtonLink href="https://github.com/Wesleygmssa/estudos-nodejs-conceitos-gobaber">Back-End</ButtonLink>
+                        </div>
+                   </CardContent>
               </Card>    
       </Cards>
     </Content>
