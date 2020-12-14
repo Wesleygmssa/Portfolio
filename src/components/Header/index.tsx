90d5dac20efb75f7e50d5ecd1101c4e1292914c5
wesley guerra <guerra_ssa@hotmail.com>
2020-12-14 15:28:04 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,19 @@
-import { useCallback,useState } from 'react';
-import {Link} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 import {HeaderContainer} from './styles';
 
 const Header: React.FC = () =>{
-const [MenuSelect, setMenuSelect] = useState('sobre');
-
-
-
-const clickMenu = useCallback((nameMenu: string) => {
-setMenuSelect(nameMenu);
-}, []);
-
 
 return(
      
     <HeaderContainer>
     <div>
-        <Link to="/" 
-             onClick={() => clickMenu('sobre')} 
-             className={MenuSelect === 'sobre' ? 'menuActive' : 'menuInactive'}>
+        <NavLink exact to="/" activeClassName='menuActive' >
             Sobre
-         </Link>
+         </NavLink>
         
-        <Link to="/projects" 
-             onClick={() => clickMenu('projetos')} 
-             className={MenuSelect === 'projetos' ? 'menuActive' : 'menuInactive'}>
+        <NavLink to="/projects" activeClassName='menuActive' >
             Projetos
-         </Link>
+         </NavLink>
     </div>
       </HeaderContainer>
 )
