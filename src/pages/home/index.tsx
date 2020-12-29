bf4808ad6afe9465af81f9126cc111c570b5764a
wesley guerra <guerra_ssa@hotmail.com>
2020-12-29 17:22:03 -0300

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -68,15 +68,14 @@ const Home: React.FC = () => {
                  <img src="https://avatars1.githubusercontent.com/u/41512408?s=460&u=48175e5af2e5f24e725eb9354200350a242d0354&v=4" alt="Wesley Guerra"/>
                  <h1 className="typing-animation">Olá, Sou Wesley Guerra 😎</h1>
                  <p>
-                     Hoje estou em busca de uma oportunidade como Dev Jr, mas sempre desenvolvia minhas próprias
-                     ideias através de cursos, então já faz um tempo que estudo sobre a programação e na verdade não
-                     mudei muito, continuo em busca de aprender mais para me possibilitar ter ideias ainda mais
-                     divertidas e desafiadoras.
+                 Hoje estou em busca de uma oportunidade com Desenvolvedor Full stack, 
+                  sempre desenvolvi minhas próprias ideias através de conhecimentos obtidos em cursos, 
+                 então já faz um tempo que estudo sobre a programação e na verdade não mudei muito, 
+                 continuo em busca de aprender mais para me possibilitar ter ideias ainda mais divertidas e desafiadoras.
+                 Comecei minha carreira com o intuito de seguir a trilha back-end, mas a verdade é que eu também gosto de fazer interfaces com React, 
+                 então decidi que me tornaria um desenvolvedor fullstack, em meio a isso fiz alguns cursos Udemy e 
+                 GoStack Bootcamp para expandi um pouco mais a minha stack de conhecimento e interesses com ReactNative.
 
-                     Comecei minha carreira com o intuito de seguir a trilha back-end, mas a verdade é que eu também
-                     gosto de fazer interfaces com React, então decidi que me tornaria um desenvolvedor fullstack, em
-                     meio a isso fiz o GoStack Bootcamp da Rocketseat e expandi um pouco mais a minha stack de
-                    conhecimento e interesses com ReactNative.
                  </p>          
                   <Button type="button" onClick={handleTogle}>Resumo Profissional</Button>       
                     {togleConteudos ? <ConteudoDois /> : <ConteudoUm />}
