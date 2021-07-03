193fedf65b6c9d920345702e9a7faa1ef1e9afb7
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-03 13:42:13 -0300

    Ajuste da barra progress

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -147,8 +147,8 @@ export const Skills = styled.div`
     align-items: center;
     justify-content: center;
 
-    & + {
-        margin-top: 50px;
+    & + div {
+        margin-top: 20px;
     }
 
     span svg {
