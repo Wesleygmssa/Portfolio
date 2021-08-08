1a492b6f5a90d59a4b18966c5caf23bae2ffbfe0
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-08 10:53:47 -0300

    Update menu

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,62 +1,57 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-  display: flex;
-  justify-content:center;
-  padding: 24px;
-  background: #293647;
-  position: relative;
+    display: flex;
+    justify-content: center;
+    padding: 24px;
+    background: #293647;
+    position: relative;
 
-
-  .menuActive{
-    transition:  2s;
-    opacity: 0.8;  
-    padding-bottom: 5px;
-    border-bottom: 2px solid var(--primary-color);
+    .menuActive {
+        transition: 2s;
+        opacity: 0.8;
+        padding-bottom: 5px;
+        border-bottom: 2px solid var(--primary-color);
     }
 
- 
+    div {
+        a {
+            font-weight: bold;
+            color: var(--primary-color);
+            font-size: 14px;
+            text-decoration: none;
+            transition: all 0.5s;
+        }
 
-  div{
+        a:hover {
+            opacity: 0.6;
+        }
 
-    a{
-      font-weight: bold;
-      color: var(--primary-color);
-      font-size: 20px;
-      text-decoration: none;
-      
-
+        a + a {
+            margin-left: 24px;
+        }
     }
-
-    a + a{
-      margin-left: 24px;
-    }
-  }
-
 `;
 
 export const Bell = styled.div`
-     padding: 5px;
-      position: absolute;
-      right: 50px;
-      display: flex;
-      justify-content:center;
-      align-items: center;
-      cursor: pointer;
-      opacity: 0.6;
-      
+    padding: 5px;
+    position: absolute;
+    right: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    opacity: 0.6;
 
-      svg{
-        color:  #fff;
-       font-size: 22px;
-      }
+    svg {
+        color: #fff;
+        font-size: 22px;
+    }
 
-
-      &:hover{
-      padding: 5px;
-      opacity: 1;
-}
-
-`
+    &:hover {
+        padding: 5px;
+        opacity: 1;
+    }
+`;
 
 // export const Link = styled.a<ContainerProps>``
