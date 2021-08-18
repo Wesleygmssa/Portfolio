63dd521c37f5011050df20b320b41df5ef1ef8b7
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-18 15:54:22 -0300

    Criando modo dark e ligth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import  GlobalStyles from './styles/global';
-import Routes from './routes';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from "./styles/global";
+import Routes from "./routes";
+import AppWrapper from "./components/AppWrapper";
 
-const  App: React.FC = () => {
- 
-  return (
-    <>
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
-    <GlobalStyles/>
-    </>
-   
-  )
-}
+const App: React.FC = () => {
+    return (
+        <AppWrapper>
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
+            <GlobalStyles />
+        </AppWrapper>
+    );
+};
 
 export default App;
