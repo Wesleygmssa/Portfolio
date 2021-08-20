fc5034b317ec856fdbeeee3ef7ab8ae92f0b2f52
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-20 06:48:07 -0300

    Ajuste do posicionamento do cione

--- src/components/ThemeSwitcher/styles.tsx
+++ src/components/ThemeSwitcher/styles.tsx
@@ -5,8 +5,8 @@ display: flex;
 align-items: center;
     position: absolute;
     margin-top: 15px;
-    top: 60px;
-    right: 2%;
+    top: 5px;
+    right: 0.5%;
 
     z-index: 10;
     transition: transform 0.9s;
