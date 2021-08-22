28c7d418ed865056cd898775eb19d2fc02441a04
Wesley Guerra <guerra_ssa@hotmail.com>
2021-08-22 08:32:20 -0300

    Removendo comentarios

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -3,17 +3,7 @@ import { useState, useEffect, Dispatch, SetStateAction } from "react";
 // type Response<T> = [T, Dispatch<SetStateAction<T>>];
 
 function usePersistedState<T>(initialState: T) {
-    // const [state, setState] = useState(() => {
-    //     const storageValue = localStorage.getItem("key");
-
-    //     if (storageValue) {
-    //         return JSON.parse(storageValue);
-    //     }
-    //     return initialState;
-    // });
-
     const [state, setState] = useState(() => {
-        // ARMAZENAMENTO USUÀRIOS LOCALSTORAGE
         const storageValue = localStorage.getItem("@theme");
 
         if (storageValue) {
